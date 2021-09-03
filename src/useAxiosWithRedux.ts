b42ca8b78e95758c8a5a441ed0cdd06acbc607d4
Johnson Tinuoye <johnsontinuoye@gmail.com>
2021-09-03 17:39:25 +0100

    Fix rerender bug when callback function is added on redux config

--- src/useAxiosWithRedux.ts
+++ src/useAxiosWithRedux.ts
@@ -1,69 +1,74 @@
-import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
-import { useState } from 'react';
-import { NetworkRequestType, ReduxConfigType } from './types';
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
+import { NetworkRequestType, ReduxConfigType } from "./types";
 
 const CancelToken = axios.CancelToken;
 const source = CancelToken.source();
 const axiosInstance = axios.create({
-    cancelToken: source.token,
+  cancelToken: source.token,
 });
 
 const useAxiosWithRedux = <RequestDataType, ResponseDataType>(
-    url: string,
-    reduxConfig: ReduxConfigType,
-    config?: AxiosRequestConfig,
+  url: string,
+  reduxConfig: ReduxConfigType,
+  config?: AxiosRequestConfig
 ): NetworkRequestType<RequestDataType, ResponseDataType> => {
-    const appDispatch = reduxConfig.appDispatch;
+  const appDispatch = reduxConfig.appDispatch;
 
-    const reduxAction = reduxConfig?.action;
-    const reduxCallback = reduxConfig?.callback;
-    const shouldStoreResponse = !!reduxAction;
-    const shouldReturnStoredResponse = !!reduxCallback;
+  const reduxAction = reduxConfig?.action;
+  const reduxCallback = reduxConfig?.callback;
+  const shouldStoreResponse = !!reduxAction;
+  const shouldReturnStoredResponse = !!reduxCallback;
 
-    const [data, setData] = useState<ResponseDataType | null>(null);
-    const [error, setError] = useState<AxiosError | null>(null);
-    const [isLoading, setIsLoading] = useState(false);
-    const [dataPromise, setDataPromise] = useState<Promise<void> | null>(null);
+  const [data, setData] = useState<ResponseDataType | null>(null);
+  const [error, setError] = useState<AxiosError | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [dataPromise, setDataPromise] = useState<Promise<void> | null>(null);
 
-    const storedResponse = reduxConfig.useSelector<ResponseDataType | void>(reduxCallback || (() => null));
+  const storedResponse = reduxConfig.useSelector<ResponseDataType | void>(
+    reduxCallback || (() => null)
+  );
+
+  const reset = (): void => {
+    setData(null);
+    setError(null);
+    setIsLoading(false);
+    setDataPromise(null);
+  };
+
+  const dispatch = (body?: RequestDataType, params?: RequestDataType) => {
+    setIsLoading(true);
+    const requestConfig = {
+      url,
+      ...config,
+      data: config?.data ? Object.assign(config?.data, body) : body,
+      params,
+    };
+    const request = axiosInstance(requestConfig)
+      .then((response: AxiosResponse) => {
+        reset();
+        setData(response.data);
+        if (shouldStoreResponse) {
+          appDispatch({ type: reduxAction, payload: response.data });
+        }
+      })
+      .catch((error: AxiosError) => {
+        reset();
+        setError(error);
+      });
+    setDataPromise(request);
+  };
+
+  const cancel = () => {
+    source.cancel("Operation cancelled by the user.");
+  };
+
+  useEffect(() => {
     if (shouldReturnStoredResponse) {
-        setData(storedResponse as ResponseDataType);
+      setData(storedResponse as ResponseDataType);
     }
+  }, [shouldReturnStoredResponse]);
 
-    const reset = (): void => {
-        setData(null);
-        setError(null);
-        setIsLoading(false);
-        setDataPromise(null);
-    };
-
-    const dispatch = (body?: RequestDataType, params?: RequestDataType) => {
-        setIsLoading(true);
-        const requestConfig = {
-            url,
-            ...config,
-            data: config?.data ? Object.assign(config?.data, body) : body,
-            params,
-        };
-        const request = axiosInstance(requestConfig)
-            .then((response: AxiosResponse) => {
-                reset();
-                setData(response.data);
-                if (shouldStoreResponse) {
-                    appDispatch({ type: reduxAction, payload: response.data });
-                }
-            })
-            .catch((error: AxiosError) => {
-                reset();
-                setError(error);
-            });
-        setDataPromise(request);
-    };
-
-    const cancel = () => {
-        source.cancel('Operation cancelled by the user.');
-    };
-
-    return [{ data, error, isLoading }, dispatch, reset, cancel, dataPromise];
+  return [{ data, error, isLoading }, dispatch, reset, cancel, dataPromise];
 };
 export default useAxiosWithRedux;
